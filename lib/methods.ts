1270668bda8b72da43a12aa40751604db248bcdb
sajjad isvand <sajjadzibaee102@gmail.com>
2024-02-20 19:29:44 +0330

    Add asynchronous and synchronous methods to read theme file

--- lib/methods.ts
+++ lib/methods.ts
@@ -3,6 +3,11 @@ import { promises as fsPromises, readFileSync } from 'node:fs'
 
 const PATH = join(__dirname, '..', 'assets', 'selorex-swagger.css')
 
+/**
+ * Reads the content of the theme file asynchronously.
+ * @returns {Promise<Buffer>} The content of the file as a Buffer.
+ * @throws {Error} If there is an error reading the file.
+ */
 export async function getThemeAsync(): Promise<Buffer> {
   try {
     return await fsPromises.readFile(PATH)
@@ -12,7 +17,12 @@ export async function getThemeAsync(): Promise<Buffer> {
   }
 }
 
-export function getTheme(): Buffer {
+/**
+ * Reads the content of the theme file synchronously.
+ * @returns {Buffer} The content of the file as a Buffer.
+ * @throws {Error} If there is an error reading the file.
+ */
+export function getThemeSync(): Buffer {
   try {
     const content = readFileSync(PATH)
     console.log('CONTENT: ', content.slice(0, 10))
